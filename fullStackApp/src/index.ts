0e4a2dd5b318369b992d0078ead2136625c16ffa
Jacob Stevenson <Jacobryanstevenson@gmail.com>
2022-02-05 16:22:43 -0600

    initial commit

---
+++ fullStackApp/src/index.ts
@@ -0,0 +1,90 @@
+import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import microConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/User";
+import { createClient } from "redis";
+import session from "express-session";
+import { __prod__ } from "./constants";
+import { MyContext } from "./types";
+import cors from "cors";
+
+const main = async () => {
+  // App Main Function
+
+  // Setup MikroORM Database
+  const orm = await MikroORM.init(microConfig);
+
+  // Migrate any Datamodel Changes to the Database
+  await orm.getMigrator().up();
+
+  // Setup Node.js Server
+  const app = express();
+
+  // Setup Redis Client Connection
+  let RedisStore = require("connect-redis")(session);
+  let redisClient = createClient();
+  redisClient.on("connect", () => {
+    console.log("Connected to Redis");
+  });
+  redisClient.on("error", (err) => {
+    console.log("Redis error: " + err);
+  });
+  redisClient.connect();
+
+  app.use(
+    cors({
+      origin: ["https://studio.apollographql.com", "http://localhost:3000"],
+      credentials: true,
+    })
+  );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+  });
+
+  await apolloServer.start();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+      },
+      secret: "falksjdfkasodjfsd", // Make Env Variable For Security
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
+  // Setup GraphQL Server
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
+
+  // Start Node Listening for Requests
+  app.listen(5000, () => {
+    console.log("web server started on localhost:5000");
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+});
---
+++ fullStackApp/src/resolvers/User.ts
@@ -0,0 +1,144 @@
+import { User } from "../entities/User";
+import argon2 from "argon2";
+
+import {
+  Resolver,
+  Arg,
+  Mutation,
+  InputType,
+  Field,
+  Ctx,
+  ObjectType,
+  Query,
+} from "type-graphql";
+import { MyContext } from "src/types";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+  @Field({ nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options", () => UsernamePasswordInput) options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "length must be greater than 2",
+          },
+        ],
+      };
+    }
+
+    if (options.password.length <= 3) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "length must be greater than 3",
+          },
+        ],
+      };
+    }
+
+    const existingUser = await em.findOne(User, { username: options.username });
+    if (existingUser) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message:
+              "user already exists, please use a unique name to register.",
+          },
+        ],
+      };
+    }
+
+    const hashedpassword = await argon2.hash(options.password);
+    console.log("Password Has was: " + hashedpassword);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedpassword,
+    });
+    await em.persistAndFlush(user);
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options", () => UsernamePasswordInput) options: UsernamePasswordInput,
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, {
+      username: options.username,
+    });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "invalid credentials",
+          },
+        ],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "invalid credentials",
+          },
+        ],
+      };
+    }
+
+    req.session!.userId = user.id;
+
+    return {
+      user,
+    };
+  }
+
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { em, req }: MyContext) {
+    if (!req.session?.userId) {
+      return null;
+    }
+
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+
+  @Query(() => [User])
+  users(@Ctx() { em }: MyContext): Promise<User[]> {
+    return em.find(User, {});
+  }
+}
